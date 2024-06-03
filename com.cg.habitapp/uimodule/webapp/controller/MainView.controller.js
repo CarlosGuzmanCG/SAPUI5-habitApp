59f6c6957c07e69e4f388c173de8310bc580df9c
CarlosGuzCG <166045537+CarlosGuzCG@users.noreply.github.com>
2024-06-02 22:26:05 -0600

    campos editables xml

--- com.cg.habitapp/uimodule/webapp/controller/MainView.controller.js
+++ com.cg.habitapp/uimodule/webapp/controller/MainView.controller.js
@@ -11,19 +11,44 @@ sap.ui.define(
 
         return Controller.extend("com.cg.habitapp.controller.MainView", {
 
-            baseRecord:  { //diccionario
+            camposTareas: [
+                'priority',
+                'title',
+                'description',
+                'date_created',
+                'date_start',
+                'due_date'
+            ],
+
+            createNewTask: function(){
+
+                var baseRecord = {};
+
+                for(var id_campo = 0; id_campo < this.camposTareas.length; id_campo++){
+                    var campo_a_agregar = this.camposTareas[id_campo];
+                    baseRecord[campo_a_agregar] = '';
+                }
+
+                return baseRecord;
+
+            },
+
+           /* baseRecord:  { //diccionario
                 priority: 0,
                 title: '',
                 description : '',
                 date_created: '',
                 date_start: '',
                 due_date: ''
-            },
+            },*/
 
             onInit: function () {
                 //var oModel = new sap.ui.model.json.JSONModel();
                 var oModel = new JSONModel(); 
                 oModel.setData([]);
+
+                oModel.loadData('/data/test_task.json');
+
                 this.setModel(oModel,'pendientes'); //Carga a la vista
                 //debbuger;
             },
@@ -32,9 +57,18 @@ sap.ui.define(
                 var oModel = this.getModel('pendientes');
                 var oDatos = oModel.getData(); // obtenemos los datos actualizados
 
-                var recordToAdd = this.baseRecord;
-                recordToAdd.date_created = new Date(); //agregamos una fecha al controlador
-                oDatos.push(this.baseRecord);
+                var recordToAdd = this.createNewTask();
+
+                var date = new Date();
+                var year = date.getFullYear();
+                var month = date.getMonth()+1;
+                var day = date.getDate();
+
+                recordToAdd.date_created = year + "-" +  month + "-" + day;
+                //recordToAdd.date_created = new Date(); //agregamos una fecha al controlador
+                recordToAdd.priority = 0;//Nuevo registro
+
+                oDatos.push( recordToAdd );
 
                 oModel.setData(oDatos);
 
@@ -54,6 +88,10 @@ sap.ui.define(
                 }
 
                 oModel.setData(oNewData);
+            },
+
+            onEdit: function(oEvent){
+                debugger
             }
         });
     });
